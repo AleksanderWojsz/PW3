4b2f973eca91793e7e8209fd5de3ba2de9156729
aw450252 <aw450252@students.mimuw.edu.pl>
2024-02-07 08:34:00 +0100

    LLQUeue From papers

--- LLQueue.c
+++ LLQueue.c
@@ -52,69 +52,35 @@ void LLQueue_delete(LLQueue* queue) {
 
 void LLQueue_push(LLQueue* queue, Value item) {
 
-    LLNode* new_node = LLNode_new(item);
+    LLNode* head = LLNode_new(item);
     while (true) {
-
         LLNode* tail = atomic_load(&queue->tail);
-        HazardPointer_protect(&queue->hp, tail);
-        if (tail != atomic_load(&queue->tail)) {
-            continue;
-        }
-
-        // Jak obecny węzeł ma pustą wartość (jest guardem) to do niego zapisujemy
-        int expected_empty = EMPTY_VALUE;
-        if (atomic_compare_exchange_strong(&tail->value, &expected_empty, item)) {
-            HazardPointer_clear(&queue->hp);
-            free(new_node);
-            return;
-        }
-
-        // Za każdym razem oczekujemy, że tail->next to będzie null.
-        // Jeśli okaże się, że jest inaczej, to znaczy, że ktoś już dodał nowy element do kolejki.
         LLNode* expected_null = NULL;
-        if (atomic_compare_exchange_strong(&tail->next, &expected_null, new_node)) {
 
-            // W tym miejscu compare_exchange ustawił tail->next na nowy węzeł.
-            if (!atomic_compare_exchange_strong(&queue->tail, &tail, new_node)) { // TODO
-                assert(0);
-            }
-
-            HazardPointer_clear(&queue->hp);
+        // Spodziewamy się, że następny node to null
+        if (atomic_compare_exchange_strong(&tail->next, &expected_null, head) == true) {
+            atomic_compare_exchange_strong(&queue->tail, &tail, head);
             return;
+        } else {
+            atomic_compare_exchange_strong(&queue->tail, &tail, tail->next);
         }
+
     }
 }
 
 
 Value LLQueue_pop(LLQueue* queue) {
 
+    LLNode *head = atomic_load(&queue->head);
     while (true) {
 
-        LLNode* head = atomic_load(&queue->head);
-        HazardPointer_protect(&queue->hp, head);
-        if (head != atomic_load(&queue->head)) {
-            continue;
-        }
-        LLNode* next = atomic_load(&head->next);
-
-        if (next == NULL) {
-            int value = atomic_load(&head->value);
-            if (atomic_compare_exchange_strong(&head->value, &value, EMPTY_VALUE)) {
-                HazardPointer_clear(&queue->hp);
-                return value;
-            } else {
-                HazardPointer_clear(&queue->hp);
-                return EMPTY_VALUE;
-            }
+        head = atomic_load(&queue->head);
+        if (head->next == NULL) {
+            return EMPTY_VALUE;
         }
 
-        int value = atomic_load(&head->value);
-        if (value != EMPTY_VALUE && atomic_compare_exchange_strong(&head->value, &value, EMPTY_VALUE)) {
-            LLNode *old_head = head;
-            atomic_store(&queue->head, next);
-            HazardPointer_clear(&queue->hp);
-//            HazardPointer_retire(&queue->hp, old_head);
-            return value;
+        if (atomic_compare_exchange_strong(&queue->head, &head, head->next) == true) {
+            return atomic_load(&head->next->value);
         }
     }
 }
@@ -123,26 +89,5 @@ Value LLQueue_pop(LLQueue* queue) {
 // Jeśli w kolejce jest tylko strażnik, to jest pusta
 bool LLQueue_is_empty(LLQueue* queue) {
 
-    while (true) {
-
-
-
-        LLNode* head = atomic_load(&queue->head);
-        HazardPointer_protect(&queue->hp, head);
-        if (head != atomic_load(&queue->head)) {
-            continue;
-        }
-        LLNode* next = atomic_load(&head->next);
-
-        if (next == NULL && atomic_load(&head->value) == EMPTY_VALUE) {
-            HazardPointer_clear(&queue->hp);
-
-            return true;
-        } else {
-            HazardPointer_clear(&queue->hp);
-
-            return false;
-        }
-    }
-
+    return atomic_load(&atomic_load(&queue->head)->next) == NULL;
 }
