df5bb9de1eb7bd944c63573b15420b59557e058e
aw450252 <aw450252@students.mimuw.edu.pl>
2024-02-10 22:57:24 +0100

    Changed test

--- testy/should_not_be_empty.c
+++ testy/should_not_be_empty.c
@@ -9,6 +9,7 @@
 #include "../LLQueue.h"
 #include "../HazardPointer.h"
 
+
 // A structure holding function pointers to methods of some queue type.
 struct QueueVTable {
     const char* name;
@@ -24,33 +25,39 @@ typedef struct QueueVTable QueueVTable;
 #pragma GCC diagnostic ignored "-Wincompatible-pointer-types"
 
 const QueueVTable queueVTables[] = {
-        { "SimpleQueue", SimpleQueue_new, SimpleQueue_push, SimpleQueue_pop, SimpleQueue_is_empty, SimpleQueue_delete },
-        { "RingsQueue", RingsQueue_new, RingsQueue_push, RingsQueue_pop, RingsQueue_is_empty, RingsQueue_delete },
-        { "LLQueue", LLQueue_new, LLQueue_push, LLQueue_pop, LLQueue_is_empty, LLQueue_delete },
+//        { "SimpleQueue", SimpleQueue_new, SimpleQueue_push, SimpleQueue_pop, SimpleQueue_is_empty, SimpleQueue_delete },
+//        { "RingsQueue", RingsQueue_new, RingsQueue_push, RingsQueue_pop, RingsQueue_is_empty, RingsQueue_delete },
+//        { "LLQueue", LLQueue_new, LLQueue_push, LLQueue_pop, LLQueue_is_empty, LLQueue_delete },
         { "BLQueue", BLQueue_new, BLQueue_push, BLQueue_pop, BLQueue_is_empty, BLQueue_delete }
 };
+
 
 #pragma GCC diagnostic pop
 
 #define THREADS 16
-#define DATA_SIZE 100
+#define DATA_SIZE 10000
 
 void* queue;
 QueueVTable Q;
 pthread_t threads[THREADS];
-Value results[THREADS][DATA_SIZE];
+_Atomic Value results[DATA_SIZE + 1];
 
 
 void* basic_test(void* thread_id)
 {
     int id = *(int*)thread_id;
     free(thread_id);
+
     HazardPointer_register(id, THREADS);
 
 
+    for (int i = 0; i < DATA_SIZE; i++) {
+        Q.push(queue, i + 1);
+    }
 
     for (int i = 0; i < DATA_SIZE; i++) {
-        assert(Q.is_empty(queue) == true);
+        assert(Q.is_empty(queue) == false);
+        results[Q.pop(queue)]++;
     }
 
 
@@ -63,6 +70,10 @@ int main(void)
         Q = queueVTables[i];
         queue = Q.new();
 
+        for (int j = 0; j < DATA_SIZE + 1; j++) {
+            results[j] = 0;
+        }
+
         for (int j = 0; j < THREADS; j++) {
             int* thread_id = malloc(sizeof(int));
             *thread_id = j;
@@ -73,11 +84,26 @@ int main(void)
             pthread_join(threads[j], NULL);
         }
 
-        Q.delete(queue);
 
         printf("Queue type: %s\n", Q.name);
+
+        bool r = false;
+        for (int j = 1; j <= DATA_SIZE; j++) {
+            if (results[j] != THREADS) {
+                printf("Liczba %d wystąpiła %lu razy zamiast %d\n", j, results[j], THREADS);
+                r = true;
+            }
+        }
+        if (r) {
+            printf("0 - %lu\n", results[0]);
+            printf("pop %ld\n", Q.pop(queue));
+
+            Q.delete(queue);
+            return 1;
+        }
+
+        Q.delete(queue);
     }
 
     return 0;
 }
-
